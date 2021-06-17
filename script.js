70a8274b0299dead5bfa3f5084ae3b744bec24f5
renatovvjr <79455144+renatovvjr@users.noreply.github.com>
2021-06-17 12:45:32 +0000

    Correção javascript

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 const CHAVE = 'api_key=c76b44768f5a56ea080472c268284709'
 const URLTHEMOVDB = 'https://api.themoviedb.org/3'
 const FILMESATUAIS = URLTHEMOVDB+'/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&'+CHAVE
 const FILMESPOPULARES = URLTHEMOVDB+'/discover/movie?sort_by=popularity.desc&'+CHAVE
 
-//PESQUISA
 const resultPesquisa = URLTHEMOVDB + '/search/company?' + CHAVE
 let mainFILME = document.getElementById('mainFILME')
 let formulario = document.getElementById('formPesquisa')
 let pesquisa = document.getElementById('pesquisa')
-//FIM PESQUISA
 
 const urlimage = "https://image.tmdb.org/t/p/w500"
 
@@ -60,15 +57,3 @@ formulario.addEventListener('submit', (e) =>{
 })
 
 
-=======
-const CHAVE = 'api_key=c76b44768f5a56ea080472c268284709'
-const URLTHEMOVDB = 'https://api.themoviedb.org/3'
-const FILMESATUAIS = URLTHEMOVDB+'/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&'+CHAVE
-const FILMESPOPULARES = URLTHEMOVDB+'/discover/movie?sort_by=popularity.desc'+CHAVE
-
-function pegarfilme(url){
-    fetch(url).then(resposta => resposta.json()).then(dados=> console.log(dados))
-}
-pegarfilme(FILMESATUAIS)
-
->>>>>>> origin/master
